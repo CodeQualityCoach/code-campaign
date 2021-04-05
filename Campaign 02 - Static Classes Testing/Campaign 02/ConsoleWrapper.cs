1d0d46b92fad1c24f24b53d4ce7d72d1d3d98ded
Thomas Ley <thomas-ley@outlook.de>
2021-04-05 21:34:14 +0200

    create campaign 02 for nunit and nsubstitute.

---
+++ Campaign 02 - Static Classes Testing/Campaign 02/ConsoleWrapper.cs
@@ -0,0 +1,10 @@
+﻿namespace Campaign_02
+{
+    public class ConsoleWrapper : IConsole
+    {
+        public void WriteLine(string output)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
